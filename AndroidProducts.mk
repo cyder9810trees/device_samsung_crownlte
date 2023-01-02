fb1d447c99751c093cf94e9f6e3d7cde1466a55a
Cyder <phamnguyengiakhiem@gmail.com>
2023-01-02 18:17:53 +0700

    crownlte: Evolution-ify
    Signed-off-by: Cyder <phamnguyengiakhiem@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crownlte.mk
+    $(LOCAL_DIR)/evolution_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crownlte-eng \
-    aosp_crownlte-user \
-    aosp_crownlte-userdebug
+    evolution_crownlte-eng \
+    evolution_crownlte-user \
+    evolution_crownlte-userdebug
